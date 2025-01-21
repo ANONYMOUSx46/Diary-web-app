ad1411e9bde0acc45a28c39aab1e52a99af9a7f0
ANONYMOUSx46 <liamjudedewet85@gmail.com>
2025-01-22 00:33:19 +0200

    Update security.js

--- security.js
+++ security.js
@@ -1,20 +1,22 @@
+import bcrypt from 'bcryptjs';
+
 export class SecurityService {
   constructor() {
     this.storageKey = 'diary-password-hash';
   }
-
+  
   async hasPassword() {
     return !!localStorage.getItem(this.storageKey);
   }
-
+  
   async setPassword(password) {
-    const hash = CryptoJS.SHA256(password).toString();
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
     localStorage.setItem(this.storageKey, hash);
   }
-
+  
   async validatePassword(password) {
     const storedHash = localStorage.getItem(this.storageKey);
-    const inputHash = CryptoJS.SHA256(password).toString();
-    return storedHash === inputHash;
+    return storedHash && await bcrypt.compare(password, storedHash);
   }
 }
